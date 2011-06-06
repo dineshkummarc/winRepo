47f4aa0ec469a0651e01cba02367b5c252470506
Irving Leonard <irvingleonard@ymail.com>
2011-06-06 15:45:58 -0400

    organizing

--- bin/jejeje.py
+++
@@ -1,9 +0,0 @@
-﻿import winPackage
-import os.path
-
-avg=winPackage.package(Path="c:\\temp")
-avg.build(Data={
-				"name":"AVG (64bit edition)",
-				"installer":"avg_free_x64_all_2011_1204a3402.exe",
-				"params":["/UILevel=Minimal","/InstallToolbar=0","/InstallSidebar=1","/ParticipateProductImprovement=0","/DontRestart","/KillProcessesIfNeeded"]
-				})
